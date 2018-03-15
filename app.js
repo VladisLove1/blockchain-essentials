019e4854885d43b1866f34616ffaac9eb275ee29
VladisLove1 <vladislove0212@gmail.com>
2018-03-15 20:33:37 +0200

    ajax-response

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+var test = {
+    t1: 'AJAX is',
+    t2: 'great'
+};
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/ajax-request', (req, res) => {
+    res.send(JSON.stringify(test));
 });
 
 app.listen(3000);
