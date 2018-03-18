d78ff42fecc8853673499bba3beb28a288165e43
VladisLove1 <vladislove0212@gmail.com>
2018-03-18 23:09:16 +0200

    connected to local blockchain and got resposnes through web3, expressjs, ganache, ajax

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-/*
+const Web3 = require('web3');
+const web3 = new Web3();
 const express = require('express');
 const app = express();
 
-var test = {
-    t1: 'AJAX is',
-    t2: 'great'
-};
+web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-});
+})
 
 app.get('/ajax-request', (req, res) => {
-    res.send(JSON.stringify(test));
+    web3.eth.getAccounts(function (error, addresses){
+        if(error == null) res.send(JSON.stringify(addresses));
+    })
 });
 
 app.listen(3000);
-*/
 
 //console.log(__dirname);
